e2c647d0e5cc331a049d23875af0911548f4fa46
MaryneGuevel22 <78147665+MaryneGuevel22@users.noreply.github.com>
2021-10-18 23:13:09 +0200

    Correction erreur

--- app/src/main/java/maryne/isis/flickr/ui/liste/MyAdaptater.java
+++ app/src/main/java/maryne/isis/flickr/ui/liste/MyAdaptater.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 import maryne.isis.flickr.model.Photo;
 
-public class MyAdaptater (val photo :List<Photo>, val callback: (Int) -> Unit) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+public class MyAdaptater {
 }
